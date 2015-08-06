a2df3c5a0f4d6aa3addec8f9a195f155a628ef6b
Alexa Zeazas & Alisa Colon <student@epicodus.com>
2015-08-06 14:55:17 -0700

    add second spec for sorting letters of compare word

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -5,4 +5,10 @@ describe('String#anagrams') do
   it("sorts letters of inputted target word alphabetically") do
     expect(("apple").anagrams()).to(eq(["a", "e", "l", "p", "p"]))
   end
+
+  it("sorts letters of inputted compare word alphabetically") do
+    expect(("paple").anagrams()).to(eq(["a", "e", "l", "p", "p"]))
+  end
+
+
 end
