9783093dd6c6d71dd28b875e3a205597f00274ab
Alexa Zeazas & Alisa Colon <student@epicodus.com>
2015-08-06 15:04:27 -0700

    Add code for sorting letters of compare word.

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,7 +1,8 @@
 class String
-  define_method(:anagrams) do
+  define_method(:anagrams) do |compare_word|
 
-  target_word = self.downcase.split(//).sort()
+  target_word = self.downcase().chars().sort()
+  compare_word = compare_word.downcase().chars().sort()
 
   target_word
 
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -3,11 +3,11 @@ require('anagrams')
 
 describe('String#anagrams') do
   it("sorts letters of inputted target word alphabetically") do
-    expect(("apple").anagrams()).to(eq(["a", "e", "l", "p", "p"]))
+    expect(("apple").anagrams("paple")).to(eq(["a", "e", "l", "p", "p"]))
   end
 
   it("sorts letters of inputted compare word alphabetically") do
-    expect(("paple").anagrams()).to(eq(["a", "e", "l", "p", "p"]))
+    expect(("apple").anagrams("ppale")).to(eq(["a", "e", "l", "p", "p"]))
   end
 
 
